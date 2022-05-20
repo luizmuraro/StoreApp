6bbdaf49988c19581c1b0e12145b58077382a1f0
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2022-05-19 21:49:28 -0300

    chore: Create colors and constats files

---
+++ src/utils/colors.ts
@@ -0,0 +1,14 @@
+export const BRAND_GREEN = '#42EC9A'
+export const GRAY_SECONDARY = '#B9B9B9'
+export const BRIGHT_RED = '#EE4B2B'
+export const WHITE = '#FFFFFF'
+
+export const GRAY_98 = '#FAFAFA'
+export const GRAY_13 = '#212121'
+export const GRAY_59 = '#979797'
+export const BLACK_0 = '#000000'
+export const GRAY_88 = '#E0E0E0'
+export const GRAY_38 = '#606060'
+
+export const POP_UP_BACKGROUND = 'rgba(39, 39, 39, 0.6)'
+export const POP_UP_SHADOW = 'rgba(39, 92, 141, 0.1)'
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const routesNames = {
+  STORE: 'Store',
+  CART: 'Cart',
+}
+
+export const ICON_SLOP_PROPS = { top: 15, right: 15, bottom: 15, left: 15 }
